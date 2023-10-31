82df3bd5ce22bce3ad5b0edfba8b5e9ae87db097
elisabichiappini <elisabichiappini@gmail.com>
2023-10-31 15:54:53 +0100

    feat: dichiarazioni e assegnazione num e input

--- myscript.js
+++ myscript.js
@@ -1,1 +1,20 @@
-// file Javascript
+// FILE JAVASCRIPT
+
+// controll Javascritp text
+'use strict'
+// controll Javascritp text
+
+// dichiarazione
+const elementName = document.getElementById ('name');
+const elementSurname = document.getElementById ('surname');
+const elementColor = document.getElementById ('color');
+const elementNumber = document.getElementById ('number');
+
+//assegnazione numero
+elementNumber.innerHTML = 21;
+
+//richiesta input al cliente
+elementName.innerHTML = prompt('Inserisci il tuo nome');
+elementSurname.innerHTML = prompt('Inserisci il tuo cognome');
+elementColor.innerHTML = prompt ('Inserisci il tuo colore');
+
